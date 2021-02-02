363020fd6eaeea4075af663b9441fbe38e01f405
Dan Sensecall <dan@sensecall.co.uk>
2021-02-02 09:55:49 +0000

    rough flow

--- app/views/case-note-1-0/routes.js
+++ app/views/case-note-1-0/routes.js
@@ -16,89 +16,12 @@ router.get('/', (req, res, next) => {
 	res.redirect(`/${req.version}/start`)
 })
 
-router.post('/create-written-note', (req, res, next) => {
-	let id = crypto.randomBytes(20).toString('hex');
-	let url = 'new-written-note?id=' + id
-
-	res.redirect(url)
-})
-
-router.post('/new-written-note', (req, res, next) => {
-	let id = req.session.data['id']
-
-	if(! req.session.data['written-notes']){
-		req.session.data['written-notes'] = new Array()
+router.post('/contact-method', (req, res, next) => {
+	if(req.session.data['contact-method'] == 'visit-not-scheduled'){
+		res.redirect('visit-time-entry')
+	} else {
+		res.redirect('contact-reason')
 	}
-
-	let note = {
-		id: id,
-		text: req.session.data['note-content'],
-		timestamp: Date.now()
-	}
-
-	req.session.data['written-notes'].push(note)
-
-	delete req.session.data['note-content']
-	delete req.session.data['id']
-
-	res.redirect('assessment-notes')
-})
-
-function getNote(req, res, id) {
-	let note = req.session.data['written-notes'].filter(function (el) {
-		return el.id === id
-	});
-
-	return note
-}
-
-router.get('/written-note', (req, res, next) => {
-	let note = getNote(req, res, req.session.data['id'])
-
-	res.render(`${req.version}/written-note`, {note})
-})
-
-router.get('/edit-written-note', (req, res, next) => {
-	let note = getNote(req, res, req.session.data['id'])
-
-	res.render(`${req.version}/edit-written-note`, {note})
-})
-
-router.post('/edit-written-note', (req, res, next) => {
-	let id = req.session.data['id']
-
-	// find the position of the matching id
-	let pos = req.session.data['written-notes'].findIndex(note => note.id == id)
-
-	// update the values
-	req.session.data['written-notes'][pos].text = req.session.data['note-content']
-	req.session.data['written-notes'][pos].timestamp = Date.now()
-
-	delete req.session.data['note-content']
-	delete req.session.data['id']
-
-	res.redirect('assessment-notes')
-})
-
-router.post('/cancel-written-note', (req, res, next) => {
-	let id = req.session.data['id']
-
-	if(! req.session.data['written-notes']){
-		req.session.data['written-notes'] = new Array()
-	}
-
-	let note = {
-		id: id,
-		text: req.session.data['note-content'],
-		timestamp: Date.now()
-	}
-
-	req.session.data['written-notes'].push(note)
-
-	delete req.session.data['note-content']
-	delete req.session.data['id']
-
-	res.redirect('assessment-notes')
 })
 
 module.exports = router
