412801a9254af3360e6bc7ee55f28c2e3ceef5db
Dan Sensecall <dan@sensecall.co.uk>
2021-02-17 09:49:42 +0000

    example events

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -22,7 +22,7 @@ module.exports = {
   "events":
   [
   {
-    'id': 'example-1',
+    'id': 'example-3',
     'type': 'Telephone call (out)',
     "datetime": "2021-02-16T12:30:00",
     'detailed-notes': 'Nam convallis eleifend ex, sed varius neque consectetur id. Praesent pellentesque aliquet massa, in fermentum lectus venenatis sed.\r\n\r\nSuspendisse congue lacus imperdiet nulla cursus pulvinar.',
@@ -31,8 +31,15 @@ module.exports = {
   {
     'id': 'example-2',
     'type': 'Home visit',
-    "datetime": "2021-01-12T15:30:00",
+    "datetime": "2021-01-22T15:30:00",
     'detailed-notes': 'Donec viverra orci velit, eget faucibus ligula porta et. Nunc at convallis mi, non ullamcorper elit.\r\n\r\nUt congue augue cursus consequat vestibulum. Duis quis ornare nisi. Aenean a malesuada justo. Phasellus porttitor mollis turpis, in lobortis nunc suscipit nec. Ut vel fringilla orci.\r\n\r\nEtiam sed lacus ut nunc suscipit venenatis sed eget odio. Nulla facilisis nec purus ut pretium.',
+    "family-members": []
+  },
+  {
+    'id': 'example-1',
+    'type': 'Referral',
+    "datetime": "2021-01-12",
+    'detailed-notes': 'Nunc at convallis mi, non ullamcorper elit. Ut congue augue cursus consequat vestibulum.\r\n\r\nDuis quis ornare nisi. Aenean a malesuada justo. Phasellus porttitor mollis turpis, in lobortis nunc suscipit nec. Ut vel fringilla orci. Etiam sed lacus ut nunc suscipit venenatis sed eget odio.\r\n\r\nNulla facilisis nec purus ut pretium.',
     "family-members": []
   }
   ]
